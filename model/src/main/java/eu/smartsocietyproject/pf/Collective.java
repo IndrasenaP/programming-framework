fe026e2a497dd7273045a446c808e2d5dcd4d69a
Tommaso Schiavinotto <tommaso.schiavinotto@gmail.com>
2016-04-27 21:49:21 +0200

    Made collective immutable, added set operations and some API docs

--- model/src/main/java/eu/smartsocietyproject/pf/Collective.java
+++ model/src/main/java/eu/smartsocietyproject/pf/Collective.java
@@ -2,37 +2,35 @@ package eu.smartsocietyproject.pf;
 
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import eu.smartsocietyproject.peermanager.Peer;
 
 import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
 public abstract class Collective {
     private final SmartSocietyApplicationContext context;
     private final String id;
-    protected final String kind;
-    private Set<Peer> members = ImmutableSet.of();
-    private Map<String, Attribute> attributes = ImmutableMap.of();
+    protected final CollectiveKind kind;
+    private final ImmutableSet<Peer> members;
+    private final ImmutableMap<String, Attribute> attributes;
 
-    public Collective(SmartSocietyApplicationContext context, String id, String kind) {
+    protected Collective(
+        SmartSocietyApplicationContext context,
+        String id,
+        CollectiveKind collectiveKind,
+        Collection<Peer> members,
+        Map<String, ? extends Attribute> attributes) {
         this.context = context;
         this.id = id;
-        this.kind = kind;
+        this.kind = collectiveKind;
+        this.members = ImmutableSet.copyOf(members);
+        this.attributes = ImmutableMap.copyOf(attributes);
     }
 
-    protected Collective(Collective from, Optional<String> kind) {
-        context = from.getContext();
-        id = from.getId();
-        this.kind = kind.orElse(from.getKind());
-        setMembers(from.getMembers());
-        setAttributes(from.getAttributes());
-    }
 
     public String getId() {
         return id;
@@ -43,34 +41,61 @@ public abstract class Collective {
     }
 
     public String getKind() {
+        return kind.getId();
+    }
+
+    public CollectiveKind getKindInstance() {
         return kind;
     }
 
-    protected Collection<Peer> getMembers() {
-        return Lists.newArrayList(members);
+    protected ImmutableSet<Peer> getMembers() {
+        return members;
     }
 
+    @Deprecated
     protected void setMembers(Collection<Peer> newMembers) {
-        members = ImmutableSet.copyOf(newMembers);
+        throw new UnsupportedOperationException("Collective classes are now immutable");
     }
 
-    protected Map<String, Attribute> getAttributes() {
-        return Maps.newHashMap(attributes);
+    public ImmutableMap<String, Attribute> getAttributes() {
+        return attributes;
     }
 
+    public Optional<Attribute> getAttribute(String name) {
+        Attribute attribute = attributes.get(name);
+        return attribute != null
+               ? Optional.of(attribute)
+               : Optional.empty();
+    }
+
+    @Deprecated
     protected void setAttributes(Map<String, Attribute> newAttributes) {
-        attributes = ImmutableMap.copyOf(newAttributes);
+        throw new UnsupportedOperationException("Collective class are now immutable");
     }
 
     protected MoreObjects.ToStringHelper toStringHelper() {
         return MoreObjects
             .toStringHelper(this)
-            .add("context", context)
+            .add("context", context.getId())
             .add("id", id)
-            .add("kind", kind)
+            .add("kind", kind.getId())
             .add("members", members)
             .add("attributes", attributes);
     }
+
+    protected void checkAttributes(Map<String, ? extends Attribute> attributes) throws CollectiveCreationException {
+        for (Map.Entry<String, ? extends Attribute> entry : attributes.entrySet()) {
+            if (!getKindInstance().isAttributeValid(entry.getKey(), entry.getValue())) {
+                throw new CollectiveCreationException(
+                    String.format(
+                        "Attribute [%s] has a not valid value [%s] for kind [%s]",
+                        entry.getKey(),
+                        entry.getValue(),
+                        getKind()));
+            }
+        }
+    }
+
 
     @Override
     public String toString() {
@@ -90,6 +115,173 @@ public abstract class Collective {
                 Objects.equal(this.context, that.context) &&
                 Objects.equal(this.members, that.members) &&
                 Objects.equal(this.attributes, that.attributes);
+    }
+
+    public abstract ApplicationBasedCollective toApplicationBasedCollective();
+
+    /**
+     * Joins two collective members, favouring the primary collective w.r.t to collective kind, attributes an user
+     * attributes
+     *
+     * @param primary   the collective that will drive the choice of kind, attributes (accordingly with the kind) and
+     *                  user attributes
+     * @param secondary the collective that will only provide members to be joined
+     * @return A new collective which members is the union of the two collective; the collective kind  and attributes
+     * are
+     * those of the primary collective;
+     * user attributes (if present) are taken from the primary collective.
+     */
+    public static ApplicationBasedCollective join(Collective primary, Collective secondary) {
+
+        try {
+            return join(primary, secondary, Optional.empty());
+        } catch (CollectiveCreationException e) {
+            throw new IllegalStateException("Set operation without toKind failed", e);
+        }
+    }
+
+    /**
+     * Joins two collective members, changing the resulting collective kind to the provided one, and favouring the
+     * primary collective w.r.t. attributes and user attributes
+     *
+     * @param primary   the collective that will drive the choice of attributes (accordingly with the kind) and user
+     *                  attributes
+     * @param secondary the collective that will only provide members to be joined
+     * @param toKind    the resulting collective kind
+     * @return A new collective which members is the union of the two collective; the kind is the one provided; the attributes are
+     * those of the primary collective that are consistent with the resulting kind, otherwise collective kind
+     * defaults are used;
+     * user attributes (if present) are taken from the primary collective.
+     * @throws CollectiveCreationException if the toKind is not known to the collective context
+     */
+    public static ApplicationBasedCollective join(Collective primary, Collective secondary, String toKind)
+        throws CollectiveCreationException {
+        Preconditions.checkNotNull(toKind);
+        return join(primary, secondary, Optional.of(toKind));
+    }
+
+    /**
+     * Joins two collective members, favouring the primary collective w.r.t to collective kind, attributes an user
+     * attributes.
+     *
+     * @param primary   the collective that will drive the choice of kind, attributes (accordingly with the kind) and
+     *                  user attributes
+     * @param secondary the collective that will only provide members to be joined
+     * @return A new collective which members is the union of the two collective; if the optional collective kind is
+     * provided it will be used instead of the primary collective kind; the attributes are
+     * those of the primary collective that are consistent with the resulting kind, otherwise collective kind
+     * defaults are used;
+     * user attributes (if present) are taken from the primary collective.
+     * @throws CollectiveCreationException if the toKind is provided but not known
+     */
+    public static ApplicationBasedCollective join(
+        Collective primary,
+        Collective secondary,
+        Optional<String> optionalKind)
+        throws CollectiveCreationException {
+        Preconditions.checkNotNull(primary);
+        Preconditions.checkNotNull(secondary);
+        Preconditions.checkNotNull(optionalKind);
+        Preconditions.checkState(primary.getContext() == secondary.getContext());
+
+        String toKind = optionalKind.orElse(primary.getKind());
+
+        ApplicationBasedCollective primaryOperable =
+            primary.toApplicationBasedCollective()
+                   .copy(toKind);
+        ApplicationBasedCollective secondaryOperable =
+            secondary.toApplicationBasedCollective()
+                     .copy(toKind)
+                     .withOnlyUserAttributes(ImmutableMap.of());
+
+        primaryOperable = primaryOperable.copy(toKind);
+        secondaryOperable = secondaryOperable.copy(toKind);
+
+        return primaryOperable.intersection(secondaryOperable);
+    }
+
+    /**
+     * It creates a new collective removing members of the secondary collective from the primary ones,
+     * favouring the primary collective w.r.t to collective kind, attributes an user
+     * attributes.
+     *
+     * @param primary   the collective that will drive the choice of kind, attributes and
+     *                  user attributes
+     * @param secondary the collective that will only provide members to be joined
+     * @return A new collective which contains the members from the primary collective that do not belong to the
+     * secondary;
+     * the collective kind and attributes are
+     * those of the primary collective;
+     * user attributes (if present) are taken from the primary collective.
+     */
+    public static ApplicationBasedCollective complement(Collective primary, Collective secondary) {
+
+        try {
+            return complement(primary, secondary, Optional.empty());
+        } catch (CollectiveCreationException e) {
+            throw new IllegalStateException("Set operation without toKind failed", e);
+        }
+    }
+
+    /**
+     * It creates a new collective removing members of the secondary collective from the primary ones with the
+     * provided kind,
+     * favouring the primary collective w.r.t to  attributes an user
+     * attributes.
+     *
+     * @param primary   the collective that will drive the choice of attributes (accordingly with the kind) and user
+     *                  attributes
+     * @param secondary the collective that will only provide members to be joined
+     * @return A new collective which contains the members from the primary collective that do not belong to the
+     * secondary;
+     * the kind is the one provided; the attributes are
+     * those of the primary collective that are consistent with the resulting kind, otherwise collective kind
+     * defaults are used;
+     * user attributes (if present) are taken from the primary collective.
+     */
+    public static ApplicationBasedCollective complement(Collective primary, Collective secondary, String toKind)
+        throws CollectiveCreationException {
+
+        return complement(primary, secondary, Optional.empty());
+    }
+
+    /**
+     * It creates a new collective removing members of the secondary collective from the primary ones with the
+     * optional kind if provided otherwise using the master ons,
+     * favouring the primary collective w.r.t to  attributes an user
+     * attributes.
+     *
+     * @param primary   the collective that will drive the choice of kind, attributes (accordingly with the kind) and user
+     *                  attributes
+     * @param secondary the collective that will only provide members to be joined
+     * @return A new collective which containing the members from the primary collective after removing those present
+     * also in the secondary; if the optional collective kind is
+     * provided it will be used instead of the primary collective kind; the attributes are
+     * those of the primary collective that are consistent with the resulting kind, otherwise collective kind
+     * defaults are used;
+     * user attributes (if present) are taken from the primary collective.
+     */
+    public static ApplicationBasedCollective complement(
+        Collective primary,
+        Collective secondary,
+        Optional<String> optionalKind)
+        throws CollectiveCreationException {
+        Preconditions.checkNotNull(primary);
+        Preconditions.checkNotNull(secondary);
+        Preconditions.checkNotNull(optionalKind);
+
+        String toKind = optionalKind.orElse(primary.getKind());
+
+        ApplicationBasedCollective primaryOperable =
+            primary.toApplicationBasedCollective()
+                   .copy(toKind);
+        ApplicationBasedCollective secondaryOperable =
+            secondary.toApplicationBasedCollective()
+                     .copy(toKind)
+                     .withOnlyUserAttributes(ImmutableMap.of());
+
+
+        return primaryOperable.intersection(secondaryOperable);
     }
 
     @Override
--- model/src/main/java/eu/smartsocietyproject/pf/ResidentCollective.java
+++ model/src/main/java/eu/smartsocietyproject/pf/ResidentCollective.java
@@ -1,28 +1,50 @@
 package eu.smartsocietyproject.pf;
 
 import eu.smartsocietyproject.peermanager.Peer;
+import eu.smartsocietyproject.peermanager.PeerManager;
 import eu.smartsocietyproject.peermanager.PeerManagerException;
 import eu.smartsocietyproject.peermanager.PeerQuery;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
 
 public final class ResidentCollective extends Collective {
-    public ResidentCollective(SmartSocietyApplicationContext context, String id, String kind) {
-        super(context, id, kind);
+    private ResidentCollective(
+        SmartSocietyApplicationContext context,
+        String id,
+        CollectiveKind kind,
+        Collection<Peer> members,
+        Map<String, Attribute> attributes) {
+        super(context, id, kind, members, attributes);
     }
 
-    public static Collection<Peer> createFromQuery(
+    public ApplicationBasedCollective toApplicationBasedCollective() {
+        try {
+            return
+                ApplicationBasedCollective
+                    .of(this.getContext(), this.getId(), this.getKindInstance(), this.getMembers())
+                    .withAttributes(getAttributes());
+        } catch (CollectiveCreationException e) {
+            throw new IllegalStateException(
+                String.format(
+                    "Failed creation of an Application Based Collective from a Resident Collective: %s",
+                    toString()), e);
+        }
+    }
+
+    public static ResidentCollective createFromQuery(
         SmartSocietyApplicationContext context,
         PeerQuery query) throws PeerManagerException {
-
+        PeerManager peerManager = context.getPeerManager();
         throw new UnsupportedOperationException("Still to be implemented");
     }
 
-    public static Collection<Peer> createFromId(
+    public static ResidentCollective createFromId(
         SmartSocietyApplicationContext context,
         String id,
         Optional<String> verify_kind) throws PeerManagerException {
+        PeerManager peerManager = context.getPeerManager();
 
         throw new UnsupportedOperationException("Still to be implemented");
     }
